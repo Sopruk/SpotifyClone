ff722e64125b607afd0f925854d5c77c22a66474
Sopruk <alexandr.programer@gmail.com>
2021-10-08 23:41:47 +0300

    added connection to mongoDb

---
+++ src/track/schemas/track.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, ObjectId } from 'mongoose';
+import * as mongoos from 'mongoose';
+import { Comment } from './comment.schema';
+
+export type TrackDocument = Track & Document;
+
+@Schema()
+export class Track {
+  @Prop()
+  name: string;
+
+  @Prop()
+  artist: string;
+
+  @Prop()
+  track: string;
+
+  @Prop()
+  listens: number;
+
+  @Prop()
+  picture: string;
+
+  @Prop()
+  audio: string;
+
+  @Prop({ type: [{ type: mongoos.Schema.Types.ObjectId, ref: 'Comment' }] })
+  comment: Comment[];
+}
+
+export const TrackSchema = SchemaFactory.createForClass(Track);
--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -2,12 +2,16 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('/tracks')
 export class TrackController {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   create() {}
   @Get()
   getAll() {
     return 'WORK';
   }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   getOne() {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   update() {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   delete() {}
 }
