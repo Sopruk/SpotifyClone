cdde07e374e629fa28dff395e9778913a3424b81
Sopruk <alexandr.programer@gmail.com>
2021-10-24 15:52:58 +0300

    added packege serve-static

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { FIleModule } from './file/file.module';
 import { TrackModule } from './track/track.module';
 
@@ -8,6 +10,9 @@ import { TrackModule } from './track/track.module';
     MongooseModule.forRoot(
       'mongodb+srv://admin:admin@cluster0.q2axr.mongodb.net/music-platform?retryWrites=true&w=majority',
     ),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
     TrackModule,
     FIleModule,
   ],
